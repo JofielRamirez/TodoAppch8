3b458145bcf61f79e4b4e4d5379077c264b48343
JofielRamirez <jofiel.ramirez@uabc.edu.mx>
2026-06-26 04:02:49 -0700

    More TodoViewModel tests

--- app/src/test/java/com/fixnow/todoappch8/todoapp/TodoViewModelTest.kt
+++ app/src/test/java/com/fixnow/todoappch8/todoapp/TodoViewModelTest.kt
@@ -28,13 +28,46 @@ class TodoViewModelTest {
     }
 
     @Test
-    fun addTask_withValidTitle_appearsInList_ExtraChallenge(){
-
-        viewModel.addTask("Do Homework")
-
-        assertEquals(1, viewModel.getTaskCount())
-        assertTrue(viewModel.containsTask("Do Homework"))
+    fun addTask_withBlankTitle_isIgnore(){
+        viewModel.addTask(" ")
+        assertEquals(0, viewModel.getTaskCount())
     }
 
+    // Test addTask with EmptyString
+    @Test
+    fun addTask_withEmptyString_isIgnored(){
+        viewModel.addTask("")
+        assertEquals(0, viewModel.getTaskCount())
+    }
+
+    //Test for Title is Trimmed
+    @Test
+    fun addTask_titleIsTrimmed() {
+        // Loading and trailing spaces should be removed
+        viewModel.addTask("Reply on Discussion 02       ")
+        assertTrue(viewModel.containsTask("Reply on Discussion 02"))
+    }
+
+    @Test
+    fun removeTask_withValidId_removesTask(){
+        // Arrange -- add a task so we can remove it
+        viewModel.addTask("Buy Groceries")
+        val taskId = viewModel.tasks[0].id
+
+        // Act -- call the function we want to test
+        viewModel.removeTask(taskId)
+
+        // Assert -- check the result
+        assertEquals(0, viewModel.getTaskCount())
+    }
+
+    // challenge removing a non-existing task ID should not crash the app or affect the list
+    @Test
+    fun removeTask_withInvalidId_doesNothing(){
+        // Removing a non-existent id should not crash or affect the list
+        viewModel.addTask("Buy groceries")
+        viewModel.removeTask(999) // id 999 does not exist
+        assertEquals(1, viewModel.getTaskCount())
+    }
 
 }
